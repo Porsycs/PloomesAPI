9d1cd703999a4d20824dd1ae5391772403a4f268
Victor Bueno Sant Anna <vicporsy@gmail.com>
2023-04-24 02:04:36 -0300

    migration
    migration

---
+++ PloomesAPI/Migrations/20230424044643_completando-cliente.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PloomesAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class completandocliente : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CPF",
+                table: "Clientes",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Cidade",
+                table: "Clientes",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Clientes",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Endereco",
+                table: "Clientes",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Estado",
+                table: "Clientes",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Nascimento",
+                table: "Clientes",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Nome",
+                table: "Clientes",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CPF",
+                table: "Clientes");
+
+            migrationBuilder.DropColumn(
+                name: "Cidade",
+                table: "Clientes");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Clientes");
+
+            migrationBuilder.DropColumn(
+                name: "Endereco",
+                table: "Clientes");
+
+            migrationBuilder.DropColumn(
+                name: "Estado",
+                table: "Clientes");
+
+            migrationBuilder.DropColumn(
+                name: "Nascimento",
+                table: "Clientes");
+
+            migrationBuilder.DropColumn(
+                name: "Nome",
+                table: "Clientes");
+        }
+    }
+}
