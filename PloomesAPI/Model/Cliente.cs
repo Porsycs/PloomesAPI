906025ba1fb77ac473f0a4143951d442afdb3f80
Victor Bueno Sant Anna <vicporsy@gmail.com>
2023-04-24 02:03:28 -0300

    cliente
    classe cliente criada

--- PloomesAPI/Model/Cliente.cs
+++ PloomesAPI/Model/Cliente.cs
@@ -4,6 +4,12 @@ namespace PloomesAPI.Common
 {
     public class Cliente : PloomesCommon
 	{
-
+		public string Nome { get; set; }
+		public string CPF { get; set; }
+		public string Nascimento { get; set; }
+		public string Endereco { get; set; }
+		public string Cidade { get; set; }
+		public string Estado { get; set; }
+		public string Email { get; set; }
 	}
 }
