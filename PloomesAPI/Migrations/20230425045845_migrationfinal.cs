4c1c97f46ed56b8a312550ee7458269c54210b4c
Victor Bueno Sant Anna <vicporsy@gmail.com>
2023-04-25 01:59:09 -0300

    final migration
    migration final token

---
+++ PloomesAPI/Migrations/20230425045845_migrationfinal.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PloomesAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class migrationfinal : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tokens",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Authenticated = table.Column<bool>(type: "bit", nullable: false),
+                    Created = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Expiration = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AccessToken = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RefreshToken = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CodigoExterno = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Inclusao = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Alteracao = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Excluido = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tokens", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tokens");
+        }
+    }
+}
